b78b04387a0ab638dbf947e070fd2d9e58d23e02
Andreas Tsarouchas <andreastsar@gmail.com>
2022-04-18 17:31:48 +0300

    udemy exercise

---
+++ ComplexOperations/src/models/ComplexNumber.java
@@ -0,0 +1,37 @@
+package models;
+
+public class ComplexNumber {
+    private double real;
+    private double imaginary;
+
+    public ComplexNumber(double real, double imaginary) {
+        this.real = real;
+        this.imaginary = imaginary;
+    }
+
+    public double getReal() {
+        return real;
+    }
+
+    public double getImaginary() {
+        return imaginary;
+    }
+
+    public void add(double real, double imaginary) {
+        this.real += real;
+        this.imaginary += imaginary;
+    }
+
+    public void add(ComplexNumber complexNumber) {
+        add(complexNumber.getReal(), complexNumber.getImaginary());
+    }
+
+    public void subtract(double real, double imaginary) {
+        this.real -= real;
+        this.imaginary -= imaginary;
+    }
+
+    public void subtract(ComplexNumber complexNumber) {
+        subtract(complexNumber.getReal(), complexNumber.getImaginary());
+    }
+}
